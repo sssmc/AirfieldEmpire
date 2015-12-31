b703b129a553f8983c369db9868d085715947a0a
Sebastien Robitaille <Sebastien Robitaille>
2015-12-31 14:31:15 -0500

    first commit

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	public Vector2 tilePosition = new Vector2();
+	private GameObject placeTileObject;
+	// Use this for initialization
+	void Start () {
+
+		placeTileObject = GameObject.Find ("Scripts");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		placeTileObject.GetComponent<placeTile> ().ReplaceTile (tilePosition);
+	}
+}
---
+++ Assets/Scripts/placeTile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class placeTile : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject grass;
+	public int offset;
+	public Vector2 worldSize;
+	GameObject[,] tiles;
+	void Start () {
+
+		tiles = new GameObject[(int)worldSize.x,(int)worldSize.y];
+
+		for (int i = 0; i < (int)worldSize.x; i++) 
+		{
+			for(int r = 0; r < (int)worldSize.y; r++)
+			{
+				tiles[i,r] = (GameObject)Instantiate(grass, new Vector3(offset * r, 0.0f, offset * i), this.transform.rotation);
+				tiles[i,r].GetComponent<Tile>().tilePosition = new Vector2(i,r);
+				//tiles[i,r].transform.Rotate(-90,0,0);
+			}
+		}
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ReplaceTile(Vector2 tileLocation)
+	{
+		Destroy(tiles[(int)tileLocation.x, (int)tileLocation.y]);
+
+	}
+}
