6122428ca4ee21bc721a0d6d344ba5dc055b76d6
Sebastien Robitaille <Sebastien Robitaille>
2015-12-31 17:44:36 -0500

    Add toolbar script and some resources folders

---
+++ Assets/Scripts/toolbar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class toolbar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject[] roads = (GameObject[])Resources.LoadAll ("tile_roads");
+		Debug.Log("roads: " + roads[0]);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
