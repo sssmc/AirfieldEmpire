c38c35808a822ef4eddb9a5d0fda7b05612e9762
Sebastien Robitaille <Sebastien Robitaille>
2015-12-31 23:10:59 -0500

    add basic toolbar ui buttons, add basic funtion to the buttons

---
+++ Assets/Scripts/toobarButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class toobarButton : MonoBehaviour {
+
+	public GameObject tile;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick()
+	{
+		Debug.Log ("Click");
+	}
+}
--- Assets/Scripts/toolbar.cs
+++ Assets/Scripts/toolbar.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class toolbar : MonoBehaviour {
+
+	public GameObject toolbarButton;
+	public GameObject toolbarCanvas;
+	public int buttonXOffset;
+	public int buttonYOffset;
 
 	// Use this for initialization
 	void Start () {
 
 		Object[] roads = Resources.LoadAll ("tile_roads");
-		Debug.Log("roads: " + roads[0]);
-
 		Object[] ground = Resources.LoadAll ("tile_ground");
-		Debug.Log("roads: " + ground[0]);
-		Debug.Log("roads: " + ground[1]);
-	
+
+		for (int i = 0; i < ground.Length; i++) 
+		{
+			GameObject button = Instantiate(toolbarButton);
+			button.transform.SetParent(toolbarCanvas.transform);
+			RectTransform buttonTransform = button.GetComponent<RectTransform>();
+			if(i == 0)
+			{
+				buttonTransform.anchoredPosition = new Vector2 (buttonXOffset, buttonYOffset);
+			}
+			else
+			{
+				buttonTransform.anchoredPosition = new Vector2 (buttonTransform.sizeDelta.x + (buttonXOffset * 2), buttonYOffset);
+			}
+			GameObject buttonText = button.transform.GetChild(0).gameObject;
+			buttonText.GetComponent<Text>().text = ground[i].ToString();
+
+
+		}
 	}
 	
 	// Update is called once per frame
