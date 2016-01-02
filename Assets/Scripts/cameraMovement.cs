407548b170d0bbe5c14d45842acfcb0b284cd727
Sebastien Robitaille <Sebastien Robitaille>
2016-01-02 14:36:06 -0500

    add min max zoom and zoom and movment speed based on distance to terrain

--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -9,45 +9,66 @@ public class cameraMovement : MonoBehaviour {
 	public Vector3 moveTarget;
 	public float zoomSmoothAmount;
 	public float moveSmoothAmount;
+	public int maxZoom;
+	public int minZoom;
+	public int zoomSpeedDistanceDivider;
+	public int moveSpeedDistanceDivider;
+	public Camera camera;
+	private float mouseRayDistance;
 	// Use this for initialization
 
 	void Start () {
 
 		moveTarget = transform.position;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		Debug.Log (mouseRayDistance / zoomSpeedDistanceDivider);
+
 		if (Input.GetKey (KeyCode.W)) 
 		{
-			moveTarget += Vector3.forward * moveSpeed;
+			moveTarget += Vector3.forward * (moveSpeed * (mouseRayDistance / moveSpeedDistanceDivider));
 		}
 		if (Input.GetKey (KeyCode.S)) 
 		{
-			moveTarget += Vector3.back * moveSpeed;
+			moveTarget += Vector3.back * (moveSpeed * (mouseRayDistance / moveSpeedDistanceDivider));
 		}
 		if (Input.GetKey (KeyCode.A)) 
 		{
-			moveTarget += Vector3.left * moveSpeed;
+			moveTarget += Vector3.left * (moveSpeed * (mouseRayDistance / moveSpeedDistanceDivider));
 		}
 		if (Input.GetKey (KeyCode.D)) 
 		{
-			moveTarget += Vector3.right * moveSpeed;
+			moveTarget += Vector3.right * (moveSpeed * (mouseRayDistance / moveSpeedDistanceDivider));
 		}
 
 		if (Input.GetAxis ("Mouse ScrollWheel") < 0) 
 		{
-			moveTarget += Vector3.up * zoomSpeed;
+			moveTarget += Vector3.up * (zoomSpeed * (mouseRayDistance / zoomSpeedDistanceDivider));
 		}
 		if (Input.GetAxis ("Mouse ScrollWheel") > 0) 
 		{
-			moveTarget += Vector3.down * zoomSpeed;
+			moveTarget += Vector3.down * (zoomSpeed * (mouseRayDistance / zoomSpeedDistanceDivider));
 		}
 
 		transform.position = new Vector3(transform.position.x, Vector3.Lerp(transform.position, moveTarget, zoomSmoothAmount * Time.deltaTime).y, transform.position.z);
 
 		transform.position = new Vector3(Vector3.Lerp(transform.position, moveTarget, moveSmoothAmount * Time.deltaTime).x, transform.position.y, Vector3.Lerp(transform.position, moveTarget, moveSmoothAmount * Time.deltaTime).z);
+
+		transform.position = new Vector3(transform.position.x, Mathf.Clamp (transform.position.y, minZoom, maxZoom), transform.position.z);
+		moveTarget.y = Mathf.Clamp (moveTarget.y, minZoom, maxZoom);
+
+		RaycastHit hit;
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+		
+		if (Physics.Raycast(ray, out hit)) {
+			Transform objectHit = hit.transform;
+			
+			mouseRayDistance = hit.distance;
+		}
 	
 	}
 }
