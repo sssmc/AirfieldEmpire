1ef79206c633bc720dcb108054b8ba914fd2a571
Sebastien Robitaille <Sebastien Robitaille>
2016-01-07 15:21:12 -0500

    added tile highlighting

--- Assets/Scripts/tileScript.cs
+++ Assets/Scripts/tileScript.cs
@@ -7,20 +7,41 @@ public class tileScript: MonoBehaviour {
 	private GameObject placeTileObject;
 	public string name;
 	public Sprite icon;
+	public bool isHighlighted;
+	private Color startColor;
+	public bool isMouseOver;
 	// Use this for initialization
 	void Start () {
 
 		placeTileObject = GameObject.Find ("Scripts");
+		startColor = GetComponent<Renderer> ().material.color;
+		isHighlighted = false;
+		isMouseOver = false;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (isHighlighted == true && isMouseOver == false) 
+		{
+			isHighlighted = false;
+			GetComponent<Renderer> ().material.color = startColor;
+		}
 	
+	}
+	void OnMouseExit()
+	{
+		isMouseOver = false;
 	}
 
 	void OnMouseOver()
 	{
+		isMouseOver = true;
+		if (isHighlighted == false) 
+		{
+			isHighlighted = true;
+			GetComponent<Renderer> ().material.SetColor("_Color", new Color(255,0,0,1.0f));
+		}
 		if (Input.GetMouseButton (0)) 
 		{
 			placeTileObject.GetComponent<placeTile> ().ReplaceTile (tilePosition);
